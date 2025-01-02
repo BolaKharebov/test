9875c080d5f8f679b610f2eeffed1530aedc056a
Kharebov Bola <kharebovbola@gmail.com>
2025-01-02 21:27:41 +0300

    Second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Getting started with GitHub");
+        System.out.println("The second commit");
     }
 }
